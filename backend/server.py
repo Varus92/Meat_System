933afa255f804d7d29bb6ca3484663d156c316e0
flavio.spano <flavio.spano@accenture.com>
2026-01-03 17:12:59 +0100

    update

--- backend/server.py
+++ backend/server.py
@@ -32,7 +32,10 @@ origins = os.environ.get("CORS_ORIGINS", "").split(",")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "https://meatsystem-79b63.web.app",
+        "http://localhost:3000"
+    ],,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
